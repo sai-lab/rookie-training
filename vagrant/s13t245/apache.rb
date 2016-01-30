b5112789ea096f586f5deca50f3cfbda8172db64
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-01-30 20:33:18 +0900

    refs #44 文字列を囲む"を'に変更

--- vagrant/s13t245/apache.rb
+++ vagrant/s13t245/apache.rb
@@ -1,7 +1,7 @@
 package 'apache2'
 
 service 'apache2' do
-  subscribes :restart, "package[apache2]"
+  subscribes :restart, 'package[apache2]'
 end
 
  execute 'echo hello itamae 4 > /var/www/html/index.html' do
