ac2827a4baeb64edf04f47fdcc3656c9715517d7
Yuu <konyou>
2016-02-01 16:19:57 +0900

    refs #48 Itamaeのテンプレート機能

--- vagrant/s13t232/apache.rb
+++ vagrant/s13t232/apache.rb
@@ -7,3 +7,12 @@ end
 execute 'echo s13t232 > /var/www/html/index.html' do
     only_if "test -e /var/www/html/index.html"
 end
+
+template "/var/www/html/index.html" do
+    action :create
+    user 'root'
+    group 'root'
+    path '/var/www/html/index.html'
+    mode '644'
+    source 'index.html.erb'
+end
