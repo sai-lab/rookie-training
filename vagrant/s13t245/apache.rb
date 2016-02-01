825179801047fa6d617635847344c9999253e0d0
nechinechi <s13t245@stmail.eng.kagawa-u.ac.jp>
2016-02-01 17:00:18 +0900

    refs #48 Itamaeのテンプレート機能

--- vagrant/s13t245/apache.rb
+++ vagrant/s13t245/apache.rb
@@ -4,6 +4,13 @@ service 'apache2' do
   subscribes :restart, 'package[apache2]'
 end
 
- execute 'echo hello itamae 4 > /var/www/html/index.html' do
-   only_if 'test -e /var/www/html/index.html'
- end
+execute 'echo hello itamae 4 > /var/www/html/index.html' do
+  only_if 'test -e /var/www/html/index.html'
+end
+
+template 'create index' do
+  path '/var/www/html/index.html'
+  user 'root'
+  group 'root'
+  mode '644'
+end
